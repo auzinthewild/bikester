83cc9f561aa539b53a33b747754f12924f9fd7fb
Austin Bodin <austin.bodin@gmail.com>
2022-07-27 16:55:32 -0500

    shopping cart component

--- src/components/Products/Product/Product.jsx
+++ src/components/Products/Product/Product.jsx
@@ -1,45 +1,60 @@
-import React from 'react'
-import { Card, CardMedia, CardContent, CardActions, Typography, IconButton } from '@mui/material';
-import { AddShoppingCart } from '@mui/icons-material';
+import React from "react";
+import {
+  Card,
+  CardMedia,
+  CardContent,
+  CardActions,
+  Typography,
+  IconButton,
+} from "@mui/material";
+import { AddShoppingCart } from "@mui/icons-material";
 
+const Product = ({ product, onAddToCart }) => {
+  const strippedDesc = product.description.replace(/(<([^>]+)>)/gi, "");
 
-const Product = ({ product }) => {
-    
-    
   return (
-    <Card sx={{
-        maxWidth: '100%'
-    }}>
-        <CardMedia sx={{
-            height: 0,
-            paddingTop: "56.25%",
-        }} image={product.image} title={product.name}/>
-        <CardContent>
-            <div style={{display: "flex",
-                justifyContent: "space-between"}}
-            >
-                <Typography variant="h5" gutterBottom>
-                    {product.name}
-                </Typography>
-                <Typography variant="h5">
-                    {product.price}
-                </Typography>
-            </div>
-            <Typography variant="body2" color="textSecondary">
-                {product.description}
-            </Typography>
-        </CardContent>
-        <CardActions disableSpacing sx={{
-            display: "flex",
-            justifyContent: "flex-end",
-        }}>
-            <IconButton aria-label="Add to Cart">
-                <AddShoppingCart />
-            </IconButton>
-        </CardActions>
-
+    <Card
+      sx={{
+        maxWidth: "100%",
+      }}
+    >
+      <CardMedia
+        sx={{
+          height: 0,
+          paddingTop: "56.25%",
+        }}
+        image={product.image.url}
+        title={product.name}
+      />
+      <CardContent>
+        <div style={{ display: "flex", justifyContent: "space-between" }}>
+          <Typography variant="h5" gutterBottom>
+            {product.name}
+          </Typography>
+          <Typography variant="h5">
+            {product.price.formatted_with_symbol}
+          </Typography>
+        </div>
+        <Typography variant="body2" color="textSecondary">
+          {strippedDesc}
+        </Typography>
+      </CardContent>
+      <CardActions
+        disableSpacing
+        sx={{
+          display: "flex",
+          justifyContent: "flex-end",
+        }}
+      >
+        <IconButton
+          aria-label="Add to Cart"
+          onClick={() => onAddToCart(product.id, 1)}
+        >
+          <AddShoppingCart />
+        </IconButton>
+      </CardActions>
     </Card>
-  )
-}
+  );
+};
 
-export default Product
+export default Product;
--- src/components/Products/Products.jsx
+++ src/components/Products/Products.jsx
@@ -3,26 +3,7 @@ import Grid from "@mui/material/Grid";
 import Product from "./Product/Product";
 import { useTheme } from "@mui/material/styles";
 
-const products = [
-  {
-    id: 1,
-    name: "Shoes",
-    description: "Running shoes",
-    price: "$5",
-    image:
-      "https://images.complex.com/complex/images/c_scale,f_auto,q_auto,w_1920/fl_lossy,pg_1/n9mhxly6ltmwiinrmzu5/wmns-air-jordan-1-retro-high-atmpsphere-lead?fimg-ssr-default",
-  },
-  {
-    id: 2,
-    name: "Macbook",
-    description: "Apple macbook",
-    price: "$5",
-    image:
-      "https://store.storeimages.cdn-apple.com/4982/as-images.apple.com/is/macbook-air-gallery3-20201110?wid=4000&hei=3072&fmt=jpeg&qlt=90&.v=1632937845000",
-  },
-];
-
-const Products = () => {
+const Products = ({ products, onAddToCart }) => {
   const theme = useTheme();
   return (
     <main
@@ -36,7 +17,7 @@ const Products = () => {
       <Grid container justifyContent="center" spacing={4}>
         {products.map((product) => (
           <Grid item key={product.id} xs={12} sm={6} md={4} lg={3}>
-            <Product product={product} />
+            <Product product={product} onAddToCart={onAddToCart} />
           </Grid>
         ))}
       </Grid>
