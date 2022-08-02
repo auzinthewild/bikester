f02fbb5adc407775d952775fbe6975dc931b4a5e
Austin Bodin <austin.bodin@gmail.com>
2022-08-02 08:10:16 -0500

    payment form components

--- src/components/CheckoutForm/PaymentForm.jsx
+++ src/components/CheckoutForm/PaymentForm.jsx
@@ -1,7 +1,65 @@
 import React from "react";
+import { Typography, Button, Divider } from "@mui/material";
+import {
+  Elements,
+  CardElement,
+  ElementsConsumer,
+} from "@stripe/react-stripe-js";
+import { loadStripe } from "@stripe/stripe-js";
 
-function PaymentForm() {
-  return <div>PaymentForm</div>;
+import Review from "./Review";
+
+const stripePromise = loadStripe(process.env.REACT_APP_STRIPE_PIBLIC_KEY);
+
+function PaymentForm({ shippingData, checkoutToken, backStep }) {
+  const handleSubmit = async (event, elements, stripe) => {
+    event.preventDefault();
+    if (!stripe || !elements) return;
+
+    const CardElement = elements.getElement(CardElement);
+
+    const { error, paymentMethod } = await stripe.createPaymentMethod({
+      type: "card",
+      card: CardElement,
+    });
+
+    if (error) {
+      console.log(error);
+    }
+  };
+
+  return (
+    <>
+      <Review checkoutToken={checkoutToken} />
+      <Divider />
+      <Typography variant="h6" gutterBottom style={{ margin: "20px 0" }}>
+        Payment Method
+      </Typography>
+      <Elements stripe={stripePromise}>
+        <ElementsConsumer>
+          {({ elements, stripe }) => (
+            <form onSubmit={(e, elements, stripe)}>
+              <CardElement />
+              <br /> <br />
+              <div style={{ display: "flex", justifyContent: "space-between" }}>
+                <Button variant="outlined" onClick={backStep}>
+                  Back
+                </Button>
+                <Button
+                  variant="contained"
+                  type="submit"
+                  disabled={!stripe}
+                  color="primary"
+                >
+                  Pay {checkoutToken.live.subtotal.formatted_with_symbol}
+                </Button>
+              </div>
+            </form>
+          )}
+        </ElementsConsumer>
+      </Elements>
+    </>
+  );
 }
 
 export default PaymentForm;
